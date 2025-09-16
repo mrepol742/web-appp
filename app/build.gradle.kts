dc3349f7e281914eb6d73951b7f66aa234afbadd
Melvin Jones Repol <mrepol742@gmail.com>
2025-09-17 00:44:43 +0800

    fix: can now switch status bar color light/dark

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.browser)
+    implementation(libs.accompanist.systemuicontroller)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
